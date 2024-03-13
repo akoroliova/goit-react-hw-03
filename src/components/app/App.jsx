22ad52c872fb85391ab154ddda0fee4886d93caa
Oleksandra K <a.koroliova@gmail.com>
2024-03-13 19:11:59 +0200

    Solve step-2

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,17 +1,24 @@
 import './App.css';
 import initialContacts from '../../initialContacts.json';
 //import ContactForm from '../contactform/ContactForm.jsx';
-//import SearchBox from '../searchbox/SearchBox.jsx';
+import SearchBox from '../searchbox/SearchBox.jsx';
 import ContactList from '../contactlist/ContactList.jsx';
+import { useState } from 'react';
 
 function App() {
   // Щоб використати дані з локального сховища в setContacts передаємо callback функцію і з неї повертаємо дані з локального сховища або пустий масив.
   // Пропоную використати useEffect в масиві залежностей якого буде лише contacts для збереження даних в локальне сховище.
 
+  const [searchBox, setSearchBox] = useState('');
+  const filteredItems = initialContacts.filter(item =>
+    item.name.toLowerCase().includes(searchBox.toLowerCase().trim())
+  );
+
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactList initialContacts={initialContacts} />
+      <SearchBox searchValue={searchBox} onFilter={setSearchBox} />
+      <ContactList initialContacts={filteredItems} />
     </>
   );
 }
